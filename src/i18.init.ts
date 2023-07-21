f722289db044eed800b8defdb56f88f4d41df5d4
xfontr <xifre@Xifres-MacBook-Pro.local>
2023-07-21 23:23:18 +0200

    I18N: Init i18n

---
+++ src/i18.init.ts
@@ -0,0 +1,18 @@
+import { LOCALES } from "./configs/constants";
+import i18n from "./services/i18n/i18n";
+import logger from "./services/logger";
+
+const $t = i18n({
+  locale: "GB-en",
+  fallbackLocale: "GB-en",
+  anyFallback: false,
+  autoInit: true,
+  allowedLocales: LOCALES,
+  log: {
+    enabled: true,
+    loggerTool: logger("I18n").warn,
+    verbosity: ["CRITIC", "LIGHT", "SUCCESS"],
+  },
+}).useI18n();
+
+export default $t;
